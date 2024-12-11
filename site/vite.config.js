27fde22d1f582f0d094d6d0bfe4a45c80427324b
Bl20052005 <88120481+Bl20052005@users.noreply.github.com>
2024-12-11 14:29:02 -0800

    r

--- site/vite.config.js
+++ site/vite.config.js
@@ -3,5 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://ics33-quiz-server-6b7xt7nav-ab-ls-projects.vercel.app",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [react()],
 });
